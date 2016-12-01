4d172896b8f0c9a70bb5a04d3e780bc8aac30acc
kr3110 <n.nkr3110@gmail.com>
2016-11-30 20:23:40 -0500

    flash messages added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,17 +9,25 @@ end
 def destroy
   @article= Article.find(params[:id])
   @article.destroy
+  flash[:notice]= "Your article was remove"
   redirect_to action: "index"
 end
-
 def create
   @article =Article.new(article_params)
   @article.save
+ flash[:notice] = "Creation was successfull"
   redirect_to article_path(@article)
 end
-
 def show
+  @article= Article.find(params[:id]) 
+end
+def edit
   @article= Article.find(params[:id])
 end
+def update
+  @article= Article.find(params[:id])
+  @article.update(article_params)
+  flash[:notice] = "Update was succesdfull"
+  redirect_to article_path(@article)
 end
-
+end
